7f822e9a917210c5bb6a7269ab9a507b777ce239
astratow <davno_pora@mail.com>
2020-12-18 05:47:12 +0000

    sumator and bmi calculator added

---
+++ 01week/sumator.py
@@ -0,0 +1,10 @@
+# Number sumarator :)
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+#num_list = list(two_digit_number)
+num_sum = int(two_digit_number[0]) + int(two_digit_number[1])
+print(num_sum)
--- day_1/band_name_generator.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band Name Generator.\n")
-nameCity = input("What is the name of the city you grew up in?\n")
-namePet = input("What is the name of your pet?\n")
-print ("Your band name will be\n" + nameCity + " " + namePet)
