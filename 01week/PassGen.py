8f6ace1ce14bd7c9c06acc55754b3af08a6265d0
astratow <davno_pora@mail.com>
2020-12-30 04:22:11 +0000

    PassGen.py created

---
+++ 01week/PassGen.py
@@ -0,0 +1,23 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+#Shuffles letters, symbols and numbers arrays
+random.shuffle(letters)
+random.shuffle(symbols)
+random.shuffle(numbers)
+
+#Selects required amount of each character
+random_password = random.choices(letters, k = nr_letters) + random.choices(symbols, k = nr_symbols) + random.choices(numbers, k = nr_numbers)
+#Shuffles characters 
+random.shuffle(random_password)
+#Joins array of characters and prints it on the screen
+random_password = ''.join(random_password)
+print('Generated password is:')
+print(random_password)
