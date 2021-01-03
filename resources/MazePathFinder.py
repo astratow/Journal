fac030d66a99f5c27928fc155e3dd65018ee6788
astratow <davno_pora@mail.com>
2021-01-03 04:53:53 +0000

    journal updated

---
+++ resources/MazePathFinder.py
@@ -0,0 +1,28 @@
+def turn_right():
+    for step in range(3):
+        turn_left()
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+#    elif wall_on_right():
+#        if wall_in_front():
+#            turn_left()
+#            move()
+    else:
+        turn_left()
+        
+        
+
