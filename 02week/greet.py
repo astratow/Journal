ba96b3eadc0226ae4dcf863c137813b612b1431a
astratow <davno_pora@mail.com>
2021-01-13 05:58:47 +0000

    greet.py upgraded

--- 02week/greet.py
+++ 02week/greet.py
@@ -2,4 +2,9 @@ def greet():
   name = input('Hello, what is your name?\n')
   print(f'Welcome {name}!')
   print('How are you?')
-greet()
+#greet()
+def greet_with(name, location):
+  name = input('Please enter your name:\n')
+  location = input('Please tell us where are you:\n')
+  print(f'You are {name} from {location}')
+greet_with('name', 'location')
