1bf98e08101b1e4cfcbe4beee244b5dc0dfcf064
astratow <davno_pora@mail.com>
2021-01-19 05:59:17 +0000

    encoder-1.5a.py uploaded. work in progress

---
+++ 02week/encrypt-1.5a.py
@@ -0,0 +1,30 @@
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+def encrypt():
+  j = 0
+  textList = list(text)
+  for i in textList:
+    #print(i)
+    alphabetPosition = alphabet.index(i)
+    alphabetPosition += shift
+    #print(alphabetPosition)
+        if alphabetPosition > 25:
+            alphabetPosition = alphabetPosition - 26
+    
+    textList[j] = alphabet[alphabetPosition]
+    j += 1
+   textEncrypted = ''.join(textList)
+   print(f'Encoded text is {textEncrypted}')
+def decrypt():
+    
+
+if direction == encode:
+   encrypt(plain_text = text, shift_amount = shift)
+elif direction == decode:
+   decrypt(plain_text = text, shift_amount = shift)   
+else:
+   print('Error. Wrong selection')
