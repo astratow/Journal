6a4a088f6cf5284b8ef3054f6b854947feae735e
astratow <davno_pora@mail.com>
2021-02-05 05:02:01 +0000

    BlackJack.0.1.py uploaded

---
+++ 02week/BlackJack.0.1.py
@@ -0,0 +1,17 @@
+import random
+print('############### Blackjack Project #####################')
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+cards_user = []
+cards_computer = []
+def deal_card():
+  """Returns random card from the deck"""
+  card_dealt = random.choice(cards)
+  return card_dealt
+
+def calculate_score():
+  return
+def compare():
+  return
+print(cards)
+
+print(deal_card())
