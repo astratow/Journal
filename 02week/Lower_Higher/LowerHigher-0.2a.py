57458c23bf2ae07c51f23e44f4d6c2df931b4643
astratow <davno_pora@mail.com>
2021-02-23 06:30:03 +0000

    LowerHigher-0.2a.py uploaded

--- 02week/Lower_Higher/LowerHigher-0.2a.py
+++ 02week/Lower_Higher/LowerHigher-0.2a.py
@@ -15,25 +15,23 @@ while not game_over:
   
     print(f"Points: {point_count} ")
   
-  #generates first choice and pops up second choice to first in following round
+#generates first choice and pops up second choice to first in following round
     if not first_choice >= 0:
         first_choice = random.randint(0, data_length - 1)
     elif first_choice >= 0:
         first_choice = second_choice
     else:
         print('First choice could not be generated')
-  #generates second choice
+#generates second choice
     second_choice = random.randint(0, data_length - 1)
-   
-  #print(first_choice)
-  #print(second_choice)
+    
     print("############################")
   
-  #ensures second choice is not the same as first
+#ensures second choice is not the same as first
     while second_choice == first_choice:
         second_choice = random.randint(0, data_length - 1)
 
-  #prints out first_choice except follower_count
+#prints out first_choice except follower_count
     for key in game_data.data[first_choice]:
         if key != 'follower_count':
             print(game_data.data[first_choice][key])
@@ -41,7 +39,7 @@ while not game_over:
     first_choice_name = game_data.data[first_choice]["name"]
     first_choice_followers = game_data.data[first_choice]["follower_count"]
   
-  #prints second_choice except follower_count
+#prints second_choice except follower_count
     print("############################")
     for key in game_data.data[second_choice]:
         if key != 'follower_count':
@@ -51,10 +49,10 @@ while not game_over:
     second_choice_name = game_data.data[second_choice]["name"]
     second_choice_followers = game_data.data[second_choice]["follower_count"]
 
-  #print(game_data.data[second_choice])
+#print(game_data.data[second_choice])
     player_choice = input(f"Is {second_choice_name} higher? Y|N: \n").lower()
   
-  # if statement displays turn results
+#if statement displays turn results
     if game_data.data[second_choice]["follower_count"] > game_data.data[first_choice]["follower_count"] and player_choice == 'y':
         print(f'Right!\n {second_choice_name} has {second_choice_followers} followers\n{first_choice_name} has only {first_choice_followers} followers')
     elif game_data.data[second_choice]["follower_count"] < game_data.data[first_choice]["follower_count"] and player_choice == 'n':
