bc800cb07e93b28c326e6629c76b58fdb60e4127
astratow <davno_pora@mail.com>
2021-03-02 05:40:07 +0000

    coffee machine simulator begins to do something...

--- 03week/CoffeMachine/main.py
+++ 03week/CoffeMachine/main.py
@@ -1,3 +1,12 @@
 from data import MENU, resources
 print(MENU)
 print(resources)
+on = True
+while on:
+    choice = input(" What would you like? \n(e)spresso\n(l)atte\n(c)appuccino\n")
+    if choice == "off":
+        on = False
+    elif choice == "r" or choice == "report":
+        for key in resources:
+            print(key)
+            print(resources[key])
