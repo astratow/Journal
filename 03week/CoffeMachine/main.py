f77a38dce323ef7cbcaf426d1b0584b5577927f7
astratow <davno_pora@mail.com>
2021-03-05 06:17:56 +0000

    coffee machine simulator continue to do something...

--- 03week/CoffeMachine/main.py
+++ 03week/CoffeMachine/main.py
@@ -3,14 +3,39 @@ from data import MENU, resources, profit
 #print(resources)
 
 def resources_check(order_ingredients):
+    """Returns True if order can be made or False if not enough ingredients"""
     #resources_ok = True
     for item in order_ingredients:
         if order_ingredients[item] >= resources[item]:
-            print(f"Sorry there is not enough{item}.")
+            print(f"Sorry there is not enough {item}.")
             #resources_ok = False
             return False
     return True
-            
+def process_coins():
+    """Returns total inserted coins"""
+    print("Please insert coins.")
+    total = int(input("How many quarters:\n")) * 0.25
+    total += int(input("How many dimes:\n")) * 0.1
+    total += int(input("How many nickles:\n")) * 0.05
+    total += int(input("How many pennies:\n")) * 0.01
+    return total
+
+def transaction_check(money_received, drink_cost):
+    """Returns True if transaction accepted or False not enough money"""
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry, that's not enough money. Please wait for refund.")
+        return False
+def make_coffee(drink_name, order_ingredients):
+    """Deducts the required ingredients from the resources"""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}.")
 
 on = True
 while on:
@@ -23,7 +48,24 @@ while on:
         print(f"money : ${profit}")
     elif choice == "e":
         print(MENU["espresso"])
+        drink = MENU["espresso"]
+        if resources_check(drink["ingredients"]):
+            payment = process_coins()
+            transaction_check(payment, drink["cost"])
+            make_coffee("espresso", drink["ingredients"])
     elif choice == "l":
         print(MENU["latte"])
+        drink = MENU["latte"]
+        if resources_check(drink["ingredients"]):
+            payment = process_coins()
+            transaction_check(payment, drink["cost"])
+            make_coffee("latte", drink["ingredients"])
     elif choice == "c":
         print(MENU["cappuccino"])
+        drink = MENU["cappuccino"]
+        if resources_check(drink["ingredients"]):
+            payment = process_coins()
+            transaction_check(payment, drink["cost"])
+            make_coffee("cappuccino", drink["ingredients"])
+    else:
+        print("Incorrect choice.")
