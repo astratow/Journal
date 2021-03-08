d4da519f38d5d9987641d8668c64daf50c44c712
astratow <davno_pora@mail.com>
2021-03-08 05:43:31 +0000

    coffee machine simulator OOP done

--- 03week/CoffeeMachineOOP/main.py
+++ 03week/CoffeeMachineOOP/main.py
@@ -1,3 +1,28 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+#menu_item = MenuItem()
+
+#money_machine.report()
+#coffee_maker.report()
+
+on = True
+
+while on:
+    options = menu.get_items()
+    choice = input(f"What would you like to drink:\n({options})\n")
+    if choice == "off":
+        print("Thank you for using Coffee Machine Simulator. Good Bye!")
+        on = False
+    elif choice == "r":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
