003feda930117ba8478a3c23531f26d329013db7
astratow <davno_pora@mail.com>
2021-03-19 05:42:13 +0000

    Quiz app starts here

--- 03week/Day-17/classes1.py
+++ 03week/Day-17/classes1.py
@@ -2,6 +2,13 @@ class User:
     def __init__(self, user_id, username):
         self.id = user_id
         self.username = username
+        self.followers = 0
+        self.following = 0
+    
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+    
     #pass
     #pass lets having empty class
     
@@ -14,13 +21,17 @@ class User:
 user_1 = User('001', 'angela')
 user_2 = User('002', 'artur')
 
+user_1.follow(user_2)
 
-print(user_1.username, user_2.username)
+print(user_1.username, user_1.followers, user_1.following,'\n', user_2.username, user_2.followers, user_1.following)
 
 
 class Car:
     def __init__(self, seats):
         self.seats = seats
+        
+    def enter_race_mode():
+        self.seats = 2
 
 my_car = Car(5)
 # same as my_car.seats = 5
---
+++ 03week/quiz-game-start/main.py
@@ -0,0 +1,6 @@
+#Question module
+attributes:
+- text '2+3=5'
+- answer True
+initialize:
+new_q = Question('2+3=5', True)
