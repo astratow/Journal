c809e947d6743763cb932d5873a0761dec7ed1fd
astratow <davno_pora@mail.com>
2021-03-20 21:10:31 +0000

    Quiz app starts here

--- 03week/quiz-game-start/main.py
+++ 03week/quiz-game-start/main.py
@@ -1,6 +1,12 @@
-#Question module
-# attributes:
-# - text '2+3=5'
-# - answer True
-# initialize:
-# new_q = Question('2+3=5', True)
+from question_model import Question
+from data import question_data
+
+#getting questions
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]    
+    new_question = Question(question_text, question_answer)
+    question.append(new_question)
+    
+print(question_bank)
