93c8330f8ac67af904c6f3ed9a51958e3fcb742e
astratow <davno_pora@mail.com>
2021-03-26 05:53:12 +0000

    Turtle exercises

--- 03week/Day-18/Turtle-play.py
+++ 03week/Day-18/Turtle-play.py
@@ -2,6 +2,13 @@ from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
 
+i = 0
+while i < 4:
+    timmy_the_turtle.right(90)
+    timmy_the_turtle.forward(200)
+    i += 1
+    
+
 screen = Screen()
 
 screen.exitonclick()
