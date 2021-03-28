5efd30436df06f00ee606eb86793dde636df2110
astratow <davno_pora@mail.com>
2021-03-28 22:33:55 +0100

    Journal updated

--- 03week/Day-18/Turtle-play.py
+++ 03week/Day-18/Turtle-play.py
@@ -1,12 +1,13 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
+tim = Turtle()
 
-i = 0
-while i < 4:
-    timmy_the_turtle.right(90)
-    timmy_the_turtle.forward(200)
-    i += 1
+
+# i = 0
+# while i < 4:
+    # tim.right(90)
+    # tim.forward(200)
+    # i += 1
     
 
 screen = Screen()
