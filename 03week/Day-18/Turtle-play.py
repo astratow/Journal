c43e8e500722b10a00e125e416723808a9fe178c
astratow <davno_pora@mail.com>
2021-03-31 05:25:31 +0100

    Journal updated, random-walk.py finished

--- 03week/Day-18/Turtle-play.py
+++ 03week/Day-18/Turtle-play.py
@@ -1,14 +1,24 @@
-from turtle import Turtle, Screen
+import turtle as t
+import random
+tim = t.Turtle()
 
-tim = Turtle()
+t.colormode(255)
 
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+#colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+directions = [0, 90, 100, 270]
+tim.pensize(20)
+tim.speed('fastest')
 
-# i = 0
-# while i < 4:
-    # tim.right(90)
-    # tim.forward(200)
-    # i += 1
-    
+for _ in range(200):
+    tim.color(random_color())
+    tim.forward(10)
+    tim.setheading(random.choice(directions))
 
 screen = Screen()
 
