a78e760775734e138f491af38f6df12e7995aa77
astratow <davno_pora@mail.com>
2021-04-10 05:14:29 +0100

    etch a sketch app - first commit

--- 03week/Day-19/etch-a-sketch-start/main.py
+++ 03week/Day-19/etch-a-sketch-start/main.py
@@ -7,7 +7,17 @@ screen = Screen()
 def move_forwards():
     tim.forward(10)
 
+def turn_left():
+    tim.left(10)
+    
+def turn_right():
+    tim.right(10)
+
 
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+
 screen.exitonclick()
