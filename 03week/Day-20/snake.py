5f4a78ce0ac5f06e15f61664f1a48cb124734e6f
astratow <davno_pora@mail.com>
2021-04-15 06:19:02 +0100

    Snake - continued...

--- 03week/Day-20/snake.py
+++ 03week/Day-20/snake.py
@@ -4,11 +4,23 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake Game')
+screen.tracer(0)
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
 for position in starting_positions:
   new_segment = Turtle('square')
   new_segment.color('white')
+  new_segment.penup()
   new_segment.goto(position)
   segments.append(new_segment)
+
+screen.update()
+
+
+game_on = True
+
+while game_on:
+  screen.update()
+  for seg in segments:
+    seg.forward(20)
