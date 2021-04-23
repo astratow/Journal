bea5a0daadba58445e47080e1d46217c8e6df04b
astratow <davno_pora@mail.com>
2021-04-23 05:13:16 +0100

    js snake game update

--- 03week/Day-20/snake.py
+++ 03week/Day-20/snake.py
@@ -8,10 +8,12 @@ screen.tracer(0)
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
+segments = []
+
 for position in starting_positions:
   new_segment = Turtle('square')
   new_segment.color('white')
-  new_segment.penup()
+  new_segment.penup() //removes line
   new_segment.goto(position)
   segments.append(new_segment)
 
